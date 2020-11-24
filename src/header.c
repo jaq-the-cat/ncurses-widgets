cb064e36d59726a73882d8464bc8e56289b58a8e
jaq-cat <jaq.cat@protonmail.ch>
2020-11-24 09:55:10 -0300

    still segfault :^)

--- src/header.c
+++ src/header.c
@@ -14,9 +14,9 @@ NWHeader NWheader(int x, int y, char* text) {
 void NWheader_display(NWHeader *header, WINDOW *stdscr) {
     const char* line = str_repeat("-", header->width);
     char* text = "";
-    text = strcat(text, "/ ");
-    text = strcat(text, header->text);
-    text = strcat(text, " /");
+    strcat(text, "/ ");
+    strcat(text, header->text);
+    strcat(text, " /");
     mvaddstr(header->y, header->x, line);
     mvaddstr(header->y+1, header->x, text);
     mvaddstr(header->y+2, header->x, line);
