4bec62c0577d26a411217992f413c4b5372a9381
jaq-cat <jaq.cat@protonmail.ch>
2020-11-24 09:19:14 -0300

    header init

--- src/widgets.c
+++ src/widgets.c
@@ -1,7 +1,15 @@
 #include "widgets.h"
+#include <string.h>
 
 NWHeader NWheader(char* text, size_t text_len) {
-
+    NWHeader h = {
+        .x = 0,
+        .y = 0,
+        strlen(text) + 4,
+        3,
+        text,
+    };
+    return h;
 }
 
 NWText NWtext(char* text, size_t text_len) {
