7473fdc170b30a9271e02ce44122ff6928ba745f
jaq-cat <jaq.cat@protonmail.ch>
2020-11-24 10:05:00 -0300

    line gae tbh

--- src/text.c
+++ src/text.c
@@ -4,15 +4,13 @@ NWText NWtext(int x, int y, char* text) {
     NWText t = {
         .x      = x,
         .y      = y,
-        .width  = strlen(text) + 1,
-        .height = 2,
+        .width  = strlen(text),
+        .height = 1,
         .text   = text,
     };
     return t;
 }
 
 void NWtext_display(NWText *w, WINDOW *stdscr) {
-    const char* line = str_repeat("-", w->width);
     mvaddstr(w->y, w->x, w->text);
-    mvaddstr(w->y+1, w->x, line);
 }
