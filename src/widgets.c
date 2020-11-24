771021517a6359f66b1f2783509a1dfc802abd33
jaq-cat <jaq.cat@protonmail.ch>
2020-11-24 09:23:06 -0300

    toggle

--- src/widgets.c
+++ src/widgets.c
@@ -35,5 +35,12 @@ NWButton NWbutton(int x, int y, char* text, size_t text_len) {
 }
 
 NWToggle NWtoggle(int x, int y, char* text, size_t text_len) {
-
+    NWToggle t = {
+        .x      = x,
+        .y      = y,
+        .width  = strlen(text) + 4,
+        .height = 1,
+        .text   = text,
+    };
+    return t;
 }
