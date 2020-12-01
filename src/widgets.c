259b66d6d1bee81af3e7bc6ae6a3795f0c0bfd32
jaq-cat <jaq.cat@protonmail.ch>
2020-12-01 20:16:09 -0300

    width

--- src/widgets.c
+++ src/widgets.c
@@ -1,5 +1,25 @@
 #include "widgets.h"
 
 NWWidget NWwidget_new(unsigned int y, char* text, enum NWWidgetType type) {
+    NWWidget w = {
+        .x = 2,
+        .y = y,
+        .width = strlen(text) + 4,
+        .height = 1,
+        .text = text,
+    };
+    return w;
+}
 
+int get_extra_width(enum NWWidgetType type) {
+    switch (type) {
+        case WHeader:
+            return 4;
+        case WButton:
+            return 4;
+        case WToggle:
+            return 4;
+        default:
+            return 0;
+    }
 }
