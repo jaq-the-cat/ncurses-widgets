a06e520cd34cb1462526a5f25940939713510f12
jaq-cat <jaq.cat@protonmail.ch>
2020-12-01 20:22:56 -0300

    polymorphism? more like gay amirite

--- src/widgets.h
+++ src/widgets.h
@@ -24,5 +24,10 @@ typedef struct {
 
 NWWidget NWwidget_new(unsigned int, char*, enum NWWidgetType);
 
+void NWheader_display(NWWidget*, WINDOW*);
+void NWbutton_display(NWWidget*, WINDOW*);
+void NWtext_display(NWWidget*, WINDOW*);
+void NWtoggle_display(NWWidget*, WINDOW*);
+
 const char* str_repeat(char*, size_t);
 #endif
