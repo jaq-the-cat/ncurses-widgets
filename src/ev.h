601be28875b4796e902e24b618d76b7a4f6ec6f4
jaq-cat <jaq.cat@protonmail.ch>
2020-12-02 14:08:41 -0300

    evnt

---
+++ src/ev.h
@@ -0,0 +1,17 @@
+#ifndef EV_H
+#define EV_H
+
+#include "widgets.h"
+
+enum EventType {
+    ButtonPress,
+    ToggleOn,
+    ToggleOff
+};
+
+typedef struct s_Events {
+    enum EventType type;
+    NWWidget source;
+} Events;
+
+#endif
